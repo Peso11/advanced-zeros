63b564c182541f1300cd7cbcdb37f134afdff82a
Peso11 <answer0333@gmail.com>
2018-03-01 18:32:41 +0300

    rdy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var osn = base;
+  var kolvo = number;
+  var x1=0;
+  var x2=0;
+  var x3=0;
+
+  for (var i = 2; i <= base; i++) {
+      x2 = 0;
+      x3 = number;
+
+      if (osn % i === 0) {
+      x1 = 0;
+       do {
+        osn = parseInt(osn/i);
+        x1++;
+
+      }  while (osn % i === 0);
+
+       do {
+        x2 = x2 + parseInt(x3/i);
+        x3= parseInt(x3/i);
+           }  while (x3/i > 0);
+      kolvo = parseInt(Math.min(kolvo, x2/x1));
+    }
+  }
+  return kolvo;
 }
